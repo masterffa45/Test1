f2a18cbdbfaeb6516020c385949e604f165d8b59
masterffa45 <66694281+masterffa45@users.noreply.github.com>
2023-10-20 17:08:24 +0300

    Car.h

--- Car.h
+++ Car.h
@@ -1,16 +1,16 @@
 #pragma once
-//библиотеки
+//libraries
 #include <iostream>
 #include <vector>
 #include "Car.h"
 #include <string>
 
 using namespace std;
-//класс для хранения данных автомобиля 
+//a class for storing car data
 class Car {
 public:
-	vector<string> marks = { "BMW  ", "Mersedec", "Lada  ","Dodge", "Nissan" };    //вектор марок автомобилей
-	vector<short int> years = { 2014, 2011, 2007, 2022, 2000 };                  //вектор содержащий год выпуска автомобилей
-	vector<string> numbers = { "S228GH", "F445TR", "O565KI","N785MO","G224BV" };     //вектор содержащий номера автомобилей
-	vector<int> reg = { 23, 15, 34, 54, 15 };                               //вектор с регионами авто
+	vector<string> marks = { "BMW  ", "Mersedec", "Lada  ","Dodge", "Nissan" };    //vector of car brands
+	vector<short int> years = { 2014, 2011, 2007, 2022, 2000 };                  //vector containing the year of manufacture of cars
+	vector<string> numbers = { "S228GH", "F445TR", "O565KI","N785MO","G224BV" };     //vector containing car numbers
+	vector<int> reg = { 23, 15, 34, 54, 15 };                               //vector with auto regions
 };
