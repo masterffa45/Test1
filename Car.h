95adc5684b927b18490d6cfa1df4b87526c2a3f4
masterffa45 <66694281+masterffa45@users.noreply.github.com>
2023-10-24 20:26:30 +0300

    Update Car.h

--- Car.h
+++ Car.h
@@ -8,9 +8,19 @@
 using namespace std;
 //a class for storing car data
 class Car {
+private:
+	string mark;                            //mark of the car
+	unsigned short year;                    //year of issue
+	string numbers;                         //numbers of the car
 public:
-	vector<string> marks = { "BMW  ", "Mersedec", "Lada  ","Dodge", "Nissan" };    //vector of car brands
-	vector<short int> years = { 2014, 2011, 2007, 2022, 2000 };                  //vector containing the year of manufacture of cars
-	vector<string> numbers = { "S228GH", "F445TR", "O565KI","N785MO","G224BV" };     //vector containing car numbers
-	vector<int> reg = { 23, 15, 34, 54, 15 };                               //vector with auto regions
+	Car(string mark, unsigned short year, string numbers);     
+	string get_mark() { return mark; };                        //we get car mark
+	unsigned short get_year() { return year; };                //we get car year of issue
+	string get_numbers() { return numbers; };                  //we get car numbers
 };
+
+Car::Car(string mark, unsigned short year, string numbers) {
+	this->mark = mark;
+	this->year = year;
+	this->numbers = numbers;
+}
